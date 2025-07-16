7763a127c74309f82290097f7b9e9f70baccb8fd
JatinDevVytla <jatinononon@gmail.com>
2025-07-16 23:28:20 +0530

    Update hardcoded.py
    Clean output

--- hardcoded.py
+++ hardcoded.py
@@ -1,7 +1,5 @@
 import math
 a, b, c = 1, -3, 2
 D = b**2 - 4*a*c
-if D >= 0:
-    print(f"Roots: {(-b + math.sqrt(D)) / (2*a):.2f}, {(-b - math.sqrt(D)) / (2*a):.2f}")
-else:
-    print("Complex roots")
+x = lambda s: (-b + s * math.sqrt(abs(D))) / (2*a)
+print(f"{'Roots' if D >= 0 else 'Complex'}: {x(1):.2f}, {x(-1):.2f}{'' if D >= 0 else 'i'}")
