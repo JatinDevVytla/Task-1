60a497578183ec276187d7d645f1c11d2417c32c
JatinDevVytla <jatinononon@gmail.com>
2025-07-16 23:25:35 +0530

    Update keyboard.py
    Adds discriminant check

--- keyboard.py
+++ keyboard.py
@@ -3,4 +3,7 @@ a = float(input())
 b = float(input())
 c = float(input())
 D = b**2 - 4*a*c
-print((-b + math.sqrt(D)) / (2*a), (-b - math.sqrt(D)) / (2*a))
+if D >= 0:
+    print((-b + math.sqrt(D)) / (2*a), (-b - math.sqrt(D)) / (2*a))
+else:
+    print("Complex roots")
