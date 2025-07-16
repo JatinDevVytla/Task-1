7fce0dc09d5374f2b05988a88b8d868f55187007
JatinDevVytla <jatinononon@gmail.com>
2025-07-16 23:30:23 +0530

    Update file_multi.py
    Adds error handling for bad input

--- file_multi.py
+++ file_multi.py
@@ -2,8 +2,11 @@ import math
 
 with open('input.txt') as f:
     for line in f:
-        a, b, c = map(float, line.split())
-        D = b**2 - 4*a*c
-        x1 = (-b + math.sqrt(D)) / (2*a)
-        x2 = (-b - math.sqrt(D)) / (2*a)
-        print(x1, x2)
+        try:
+            a, b, c = map(float, line.split())
+            D = b**2 - 4*a*c
+            x1 = (-b + math.sqrt(D)) / (2*a)
+            x2 = (-b - math.sqrt(D)) / (2*a)
+            print(x1, x2)
+        except:
+            print("Invalid input, skipped.")
