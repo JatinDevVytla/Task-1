8616beeaacb2ddc5494ce9420051871e9220b311
JatinDevVytla <jatinononon@gmail.com>
2025-07-16 23:29:58 +0530

    Create file_multi.py
    Basic file reader

---
+++ file_multi.py
@@ -0,0 +1,9 @@
+import math
+
+with open('input.txt') as f:
+    for line in f:
+        a, b, c = map(float, line.split())
+        D = b**2 - 4*a*c
+        x1 = (-b + math.sqrt(D)) / (2*a)
+        x2 = (-b - math.sqrt(D)) / (2*a)
+        print(x1, x2)
