5c09fb5b5b06667c4de7a9b34aff5aa5a1b8755f
JatinDevVytla <jatinononon@gmail.com>
2025-07-16 23:26:31 +0530

    Update keyboard.py
    Input validation

--- keyboard.py
+++ keyboard.py
@@ -1,11 +1,14 @@
 import math
-a = float(input())
-b = float(input())
-c = float(input())
-D = b**2 - 4*a*c
-if D >= 0:
-    print(f"Roots: {(-b + math.sqrt(D)) / (2*a):.2f}, {(-b - math.sqrt(D)) / (2*a):.2f}")
-else:
-    real = -b / (2*a)
-    imag = math.sqrt(-D) / (2*a)
-    print(f"Complex: {real:.2f} ± {imag:.2f}i")
+try:
+    a = float(input("a: "))
+    b = float(input("b: "))
+    c = float(input("c: "))
+    D = b**2 - 4*a*c
+    if D >= 0:
+        print(f"Roots: {(-b + math.sqrt(D)) / (2*a):.2f}, {(-b - math.sqrt(D)) / (2*a):.2f}")
+    else:
+        real = -b / (2*a)
+        imag = math.sqrt(-D) / (2*a)
+        print(f"Complex: {real:.2f} ± {imag:.2f}i")
+except:
+    print("Invalid input")
