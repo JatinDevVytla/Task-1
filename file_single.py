53192c7bf1a54c1acf92b8bf698ba977f88256b7
JatinDevVytla <jatinononon@gmail.com>
2025-07-16 23:22:39 +0530

    Update file_single.py
    Adds complex root support

--- file_single.py
+++ file_single.py
@@ -2,6 +2,10 @@ import math
 a, b, c = map(float, open('input.txt').readline().split())
 D = b**2 - 4*a*c
 if D >= 0:
-    print((-b + math.sqrt(D)) / (2*a), (-b - math.sqrt(D)) / (2*a))
+    x1 = (-b + math.sqrt(D)) / (2*a)
+    x2 = (-b - math.sqrt(D)) / (2*a)
+    print(f"Roots: {x1:.2f}, {x2:.2f}")
 else:
-    print("Complex roots")
+    real = -b / (2*a)
+    imag = math.sqrt(-D) / (2*a)
+    print(f"Complex Roots: {real:.2f} ± {imag:.2f}i")
