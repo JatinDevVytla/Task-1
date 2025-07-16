b1ad2b5832bb90cca5a37da1e64b2d68acdcaeae
JatinDevVytla <jatinononon@gmail.com>
2025-07-16 23:28:00 +0530

    Update hardcoded.py
    Adds complex support

--- hardcoded.py
+++ hardcoded.py
@@ -1,4 +1,7 @@
 import math
 a, b, c = 1, -3, 2
 D = b**2 - 4*a*c
-print((-b + math.sqrt(D)) / (2*a), (-b - math.sqrt(D)) / (2*a))
+if D >= 0:
+    print(f"Roots: {(-b + math.sqrt(D)) / (2*a):.2f}, {(-b - math.sqrt(D)) / (2*a):.2f}")
+else:
+    print("Complex roots")
