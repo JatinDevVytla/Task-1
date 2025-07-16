c3c8fed473ec1c88acec91156a1c5bf6d8c7e896
JatinDevVytla <jatinononon@gmail.com>
2025-07-16 23:22:08 +0530

    Update file_single.py
    Discriminant check (real roots only)

--- file_single.py
+++ file_single.py
@@ -1,7 +1,7 @@
 import math
-with open('input.txt') as f:
-    a, b, c = map(float, f.readline().split())
-    D = b**2 - 4*a*c
-    x1 = (-b + math.sqrt(D)) / (2*a)
-    x2 = (-b - math.sqrt(D)) / (2*a)
-    print(x1, x2)
+a, b, c = map(float, open('input.txt').readline().split())
+D = b**2 - 4*a*c
+if D >= 0:
+    print((-b + math.sqrt(D)) / (2*a), (-b - math.sqrt(D)) / (2*a))
+else:
+    print("Complex roots")
